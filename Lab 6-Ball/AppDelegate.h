85b22fa8d8be19d64a40d5889ec8dc5eaa774249
euau <euau@iolani.org>
2013-09-27 11:37:08 -1000

    Fixed
    Should be working now

---
+++ Lab 6-Ball/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lab 6-Ball
+//
+//  Created by Eugene Au on 9/16/13.
+//  Copyright (c) 2013 Eugene Au. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
